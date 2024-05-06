d964a10d103a69909b32449489c4cc28768e6ae3
JIUK KIM <99459331+ziweek@users.noreply.github.com>
2024-05-07 01:47:16 +0900

    Time: 15 ms (26.27%), Space: 44.3 MB (45.95%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,22 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int start = 0;
-        int end = s.length() - 1;
-        
-        while (start < end) {
-            if (!Character.isLetterOrDigit(s.charAt(start))) {
-                start++;
-            } else if (!Character.isLetterOrDigit(s.charAt(end))) {
-                end--;
-            } else {
-                if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
-                    return false;
-                }
-                start++;
-                end--;
-            }
-        }
-        //
-        return true;
+        String s_filtered = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        String s_reversed = new StringBuilder(s_filtered).reverse().toString();
+        return s_filtered.equals(s_reversed);
     }
 }
