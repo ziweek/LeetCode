05b2ad61328118ff15d1a1cdeb53611003a1568b
JIUK KIM <99459331+ziweek@users.noreply.github.com>
2024-05-07 03:33:27 +0900

    Time: 4 ms (80.81%), Space: 44.9 MB (37.62%) - LeetHub

---
+++ 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String[] reorderLogFiles(String[] logs) {
+        List<String> letterList = new ArrayList<>();
+        List<String> digitList = new ArrayList<>();
+        
+        for (String log : logs) {
+            if (Character.isDigit(log.split(" ")[1].charAt(0))) {
+                digitList.add(log);
+            } else {
+                letterList.add(log);
+            }
+        }
+        
+        letterList.sort((s1, s2) -> {
+            String[] s1x = s1.split(" ", 2);
+            String[] s2x = s2.split(" ", 2);
+            
+            int compared = s1x[1].compareTo(s2x[1]);
+            
+            if (compared == 0) {
+                return s1x[0].compareTo(s2x[0]);
+            } else {
+                return compared;
+            }            
+        });
+        
+        letterList.addAll(digitList);
+        
+        return letterList.toArray(new String[0]);
+    }
+}
