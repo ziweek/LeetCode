b90d5bf9df1f8ba8fc3cb11ea9e50adaa64411de
JIUK KIM <99459331+ziweek@users.noreply.github.com>
2024-05-10 00:50:45 +0900

    Time: 11 ms (86.05%), Space: 42.8 MB (56.95%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        Set<String> ban = new HashSet<>(Arrays.asList(banned));
+        Map<String, Integer> counts = new HashMap();
+        
+        String[] words = paragraph.replaceAll("\\W+", " ").toLowerCase().split(" ");
+        
+        for (String w : words) {
+            if (!ban.contains(w)) {
+                counts.put(w, counts.getOrDefault(w,0) + 1);
+            }
+        }
+        
+        return Collections.max(counts.entrySet(), Map.Entry.comparingByValue()).getKey();
+    }
+}
