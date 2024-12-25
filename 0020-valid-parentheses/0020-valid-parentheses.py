4bb8229f9bdedde62571ab9fa57082274768c786
JIUK KIM <99459331+ziweek@users.noreply.github.com>
2024-12-26 00:45:48 +0900

    Time: 4 ms (27.51%), Space: 12.5 MB (6.69%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -11,10 +11,18 @@ class Solution(object):
             ']': '['
         }
         
+        # for char in s:
+        #     if char not in table:
+        #         stack.append(char)
+        #     elif not stack or table[char] != stack.pop():
+        #         return False
+        # return len(stack) == 0
+    
+        
         for char in s:
             if char not in table:
                 stack.append(char)
             elif not stack or table[char] != stack.pop():
-                return False
+                    return False
             # print(stack)
         return len(stack) == 0
