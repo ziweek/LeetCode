0c29daf08307716427e91b2c9e7056fc09ccf08a
JIUK KIM <99459331+ziweek@users.noreply.github.com>
2024-12-26 15:57:14 +0900

    Time: 0 ms (100.00%), Space: 13.4 MB (8.71%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -5,7 +5,20 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        b = bisect_left(nums, target)
+        l = 0
+        r = len(nums) - 1
+        result = 0
+        
+        while l <= r:
+            mid = (l+r) // 2
+            if nums[mid] >= target:
+                result = mid
+                r = mid - 1
+            else:
+                l = mid + 1
+        b = result
+        
+        # b = bisect_left(nums, target)
         print(b)
         return -1 if b >= len(nums) or nums[b] != target else b
         
