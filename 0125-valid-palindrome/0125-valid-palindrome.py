c91fc5f19382e390bc66dfa9e86aa29bf7774520
JIUK KIM <99459331+ziweek@users.noreply.github.com>
2024-12-26 14:47:21 +0900

    Time: 8 ms (91.35%), Space: 13.8 MB (36.63%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,30 +4,30 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        # s_replaced = s.lower().replace(" ", "").replace(",", "").replace(".", "").replace(":", "")
+#         s_replaced = []
+#         for e in s.lower().replace(" ", ""):
+#             if e.isalnum():
+#                 s_replaced.append(e)
         
-        # s_replaced = [e if e.isalpha() else "" for e in s.lower().replace(" ", "")]
-        # print(s_replaced)
+#         if len(s_replaced) <= 1:
+#             return True
+    
+#         i_start = 0
+#         i_end = len(s_replaced)-1 - i_start
+#         while i_start <= i_end:
+#             if s_replaced[i_start] == s_replaced[i_end]:
+#                 i_start += 1
+#                 i_end -= 1
+#                 if len(s_replaced[i_start:i_end+1]) <= 1:
+#                     return True
+#             else:
+#                 return False
+#         return False
         s_replaced = []
         for e in s.lower().replace(" ", ""):
             if e.isalnum():
                 s_replaced.append(e)
-        
-        if len(s_replaced) <= 1:
-            return True
-        
-        # print(s_replaced)
-        i_start = 0
-        i_end = len(s_replaced)-1 - i_start
-        # print(i_start, i_end)
-        while i_start <= i_end:
-            if s_replaced[i_start] == s_replaced[i_end]:
-                i_start += 1
-                i_end -= 1
-                if len(s_replaced[i_start:i_end+1]) <= 1:
-                    return True
-            else:
-                return False
-        return False
+        return s_replaced == s_replaced[::-1]
+                
             
         
