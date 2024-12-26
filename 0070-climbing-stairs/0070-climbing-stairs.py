55cc2ee727637847d04a3d6a9a3510d6a678068e
JIUK KIM <99459331+ziweek@users.noreply.github.com>
2024-12-26 21:12:42 +0900

    Time: 0 ms (100.00%), Space: 12.3 MB (18.69%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        memo = {}
+        return self.helper(n, memo)
+    
+    def helper(self, n, memo):
+        if n==0 or n==1:
+            return 1
+        if n not in memo:
+            memo[n] = self.helper(n-1, memo) + self.helper(n-2, memo)
+        return memo[n]
+
+
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         memo = {}
+#         return self.helper(n, memo)
+    
+#     def helper(self, n: int, memo: dict[int, int]) -> int:
+#         if n == 0 or n == 1:
+#             return 1
+#         if n not in memo:
+#             memo[n] = self.helper(n-1, memo) + self.helper(n-2, memo)
+#         return memo[n]
