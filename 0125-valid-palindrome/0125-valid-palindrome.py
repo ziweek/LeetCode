1b1b68207799a21b1b14cb466fb5f729ef683866
JIUK KIM <99459331+ziweek@users.noreply.github.com>
2024-12-26 14:44:23 +0900

    Time: 2075 ms (5.04%), Space: 14 MB (29.21%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        # s_replaced = s.lower().replace(" ", "").replace(",", "").replace(".", "").replace(":", "")
+        
+        # s_replaced = [e if e.isalpha() else "" for e in s.lower().replace(" ", "")]
+        # print(s_replaced)
+        s_replaced = []
+        for e in s.lower().replace(" ", ""):
+            if e.isalnum():
+                s_replaced.append(e)
+        
+        if len(s_replaced) <= 1:
+            return True
+        
+        print(s_replaced)
+        i_start = 0
+        i_end = len(s_replaced)-1 - i_start
+        # print(i_start, i_end)
+        while i_start <= i_end:
+            if s_replaced[i_start] == s_replaced[i_end]:
+                i_start += 1
+                i_end -= 1
+                if len(s_replaced[i_start:i_end+1]) <= 1:
+                    return True
+            else:
+                return False
+        return False
+            
+        
