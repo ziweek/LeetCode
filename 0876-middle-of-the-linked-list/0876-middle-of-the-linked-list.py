a34b704b254ebe5dbf401f39c71eb161a9e5f56a
JIUK KIM <99459331+ziweek@users.noreply.github.com>
2024-12-27 13:29:50 +0900

    Time: 0 ms (100.00%), Space: 12.5 MB (8.59%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -9,13 +9,28 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        result = []
-        counter = 0
-        while head != None:
-            result.append(head)
-            head = head.next
-            counter += 1
+#         result = []
+#         counter = 0
+#         while head != None:
+#             result.append(head)
+#             head = head.next
+#             counter += 1
 
-        return result[counter // 2]
+#         return result[counter // 2]
 
+        slow = head
+        fast = head
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+        # slow = head
+        # fast = head
+        # while fast is not None and fast.next is not None:
+        #     slow = slow.next
+        #     fast = fast.next.next
+        # return slow
+        # if slow == slow.next:
+        #     return slow.next
         
